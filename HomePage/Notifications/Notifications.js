0a81a750bfe311ce1f30e063284282c5c2ec7230
AhmedAlhawa <ahmadalhawa85@gmail.com>
2024-09-08 05:42:10 -0700

    Add files via upload

--- HomePage/Notifications/Notifications.js
+++ HomePage/Notifications/Notifications.js
@@ -2,3 +2,15 @@
 document.getElementById('home').addEventListener('click',()=>{
     window.location.href="../HomePage.html"
 })
+
+// chat
+const chat = document.getElementById('chat');
+chat.addEventListener("click",()=>{
+    window.location.href="../Chat/Chat.html"
+})
+
+// favorite
+document.getElementById('favorite').addEventListener('click',()=>{
+    window.location.href="../Favorite/favorite.html"
+  })
+  
